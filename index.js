610408be4b8623997276458c605dc11a542aa826
Zahran Kabir <zahrank@gmail.com>
2019-10-10 17:33:13 -0700

    solved

---
+++ index.js
@@ -0,0 +1,19 @@
+function countdown(callback){
+  window.setTimeout(callback, 2000)
+}
+
+function createMultiplier(n){
+  return function (value){
+    return value * n
+  }
+}
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
+
